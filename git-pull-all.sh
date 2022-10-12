b11b4f39f3166eb72d68217e236e2ef2ea9ea7bc
Tal Jacob <taljacob2@gmail.com>
2022-10-12 22:26:45 +0300

    Reorder Printing
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -13,6 +13,9 @@ function gitPullAllTrackedBranches() {
     # Default `$REMOTE` value is "origin".
     REMOTE="${1:-origin}"
 
+    # Log print.
+    echo "Pulling all '$REMOTE' tracked branches..."
+
     HEAD_NAME=$(git symbolic-ref --short HEAD)
 
     git add . > /dev/null 2>&1
@@ -35,11 +38,15 @@ function gitPullAllTrackedBranches() {
             `$brnameTrackStatus` is NOT empty. Thus `$brname` has a tracked
             remote branch, that can be pulled.
             '
+            echo
+            
             git checkout $brname
             git pull
         fi
         
     done
+
+    echo
 
     git checkout $HEAD_NAME > /dev/null 2>&1
 

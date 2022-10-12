c21216febcfcd3233ac138adce0456bb8bd3133d
Tal Jacob <taljacob2@gmail.com>
2022-10-12 20:27:56 +0300

    Add Debug Prints For Variables
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -12,12 +12,18 @@ function gitPullAllTrackedBranches() {
     # Default `$REMOTE` value is "origin".
     REMOTE="${1:-'origin'}"
 
+    # DEBUG
+    echo "DEBUG REMOTE: $REMOTE"
+
     HEAD_NAME=$(git symbolic-ref --short HEAD)
 
     git add .
     git stash
 
     for brname in `git branch -r | grep $REMOTE | grep -v /HEAD | awk '{gsub(/^[^\/]+\//,"",$1); print $1}'`; do
+
+        # DEBUG
+        echo "DEBUG brname: $brname"
 
         # Check if `$brname` has a tracked remote branch that can be pulled.
         brnameTrackStatus=`git remote show $REMOTE | grep "^    $brname" | grep "merges with remote"`
